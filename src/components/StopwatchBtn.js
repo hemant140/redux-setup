e0c0da4800f9e6c3c824ed38ec0a8ce70e2fe91a
hemantplutos <hemant@plutos.one>
2024-05-16 00:29:13 +0530

    btn color

--- src/components/StopwatchBtn.js
+++ src/components/StopwatchBtn.js
@@ -3,7 +3,7 @@ function StopwatchBtn({ clockBtn, startBtn, stopBtn }) {
 
     return (
         <div>
-            <button className='startbtn' onClick={startBtn}>
+            <button className='startbtn' style={{ backgroundColor: clockBtn ? "#057fcf" : "#26cf05" }} onClick={startBtn}>
                 {clockBtn ? "Pause" : "Start"}
             </button>
             <button className='startbtn' style={{ backgroundColor: "red" }} onClick={stopBtn}>Stop</button>
