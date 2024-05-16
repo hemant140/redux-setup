cbf95c659fd2ac889d377199d0e35ed561168e10
hemantplutos <hemant@plutos.one>
2024-05-16 10:04:17 +0530

    favicon

--- src/Pages/Stopwatch.js
+++ src/Pages/Stopwatch.js
@@ -4,6 +4,7 @@ import StopwatchBtn from '../components/StopwatchBtn'
 
 function Stopwatch() {
     const [timerSeconds, setTimerSeconds] = useState(0)
+    const [time, setTime] = useState(0)
     const [timerMinutes, setTimerMinutes] = useState(0)
     const [clockBtn, setClockBtn] = useState(false);
 
@@ -11,14 +12,15 @@ function Stopwatch() {
         let stopWatch;
         if (clockBtn) {
             stopWatch = setInterval(() => {
-                setTimerSeconds(previous => {
-                    if (previous === 59) {
-                        setTimerMinutes(prev => prev + 1);
-                        return 0;
-                    } else {
-                        return previous + 1;
-                    }
-                });
+                // setTimerSeconds(previous => {
+                //     if (previous === 59) {
+                //         setTimerMinutes(prev => prev + 1);
+                //         return 0;
+                //     } else {
+                //         return previous + 1;
+                //     }
+                // });
+                setTime(prev => prev + 1)
             }, 1000)
         } else {
             clearInterval(stopWatch)
@@ -35,6 +37,7 @@ function Stopwatch() {
         setClockBtn(false);
         setTimerSeconds(0);
         setTimerMinutes(0);
+        setTime(0)
     }
 
 
@@ -46,7 +49,7 @@ function Stopwatch() {
 
             <div className='stopwatch-main'>
                 <div className="stopwatch-content">
-                    <TimerDisplay timerSeconds={timerSeconds} timerMinutes={timerMinutes} />
+                    <TimerDisplay timerSeconds={timerSeconds} timerMinutes={timerMinutes} time={time} />
                     <StopwatchBtn clockBtn={clockBtn} startBtn={handleStartPause} stopBtn={handleStop} />
                 </div>
 
--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -1,10 +1,31 @@
 
-function TimerDisplay({ timerMinutes, timerSeconds }) {
+function TimerDisplay({ timerMinutes, timerSeconds, time }) {
+
+    // function formatTime(time) {
+
+    //     let minutes = Math.floor(time / 60);
+    //     let seconds = time % 60;
+    //     return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    // }
+
+    const formatTime = (time) => {
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const seconds = Math.floor(time % 60);
+
+        const formattedHours = `${hours < 10 ? '0' : ''}${hours}`;
+        const formattedMinutes = `${minutes < 10 ? '0' : ''}${minutes}`;
+        const formattedSeconds = `${seconds < 10 ? '0' : ''}${seconds}`;
+
+        return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    };
+
 
     return (
         <div>
             <h1 className='displayTimer'>
-                {timerMinutes < 10 ? '0' + timerMinutes : timerMinutes}:{timerSeconds < 10 ? '0' + timerSeconds : timerSeconds}
+                {/* {timerMinutes < 10 ? '0' + timerMinutes : timerMinutes}:{timerSeconds < 10 ? '0' + timerSeconds : timerSeconds} */}
+                {formatTime(time)}
             </h1>
         </div>
     )
